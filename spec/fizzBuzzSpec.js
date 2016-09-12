344fcd4dd7c2516d3706b3b7f58376f724c82ebf
Matthew Perkins <mfperkins@gmail.com>
2016-09-12 15:09:29 +0100

    fizzbuzz complete :)

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -10,4 +10,12 @@ describe("FizzBuzz",function() {
     expect(miniFizz.numbers()).toContain(100);
   });
 
+  it("prints 'fizz' if number is divisible by 3", function() {
+    var output = miniFizz.play();
+    expect(output[2]).toEqual("fizz");
+    expect(output[4]).toEqual("buzz");
+    expect(output[14]).toEqual("fizzbuzz");
+    expect(output[57]).toEqual(58);
+  })
+
 });
--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -10,3 +10,19 @@ FizzBuzz.prototype.numbers = function() {
   // console.log(array)
   return array
 };
+
+FizzBuzz.prototype.play = function() {
+  var array = this.numbers();
+  var output = [];
+  array.forEach(function(number) {
+    if ((number % 3 == 0) && (number % 5 == 0)) {
+     output.push("fizzbuzz");
+   } else if (number % 5 == 0) {
+     output.push("buzz");
+   } else if (number % 3 == 0) {
+     output.push("fizz");
+   } else {
+     output.push(number);
+   }
+ }); return output;
+}
